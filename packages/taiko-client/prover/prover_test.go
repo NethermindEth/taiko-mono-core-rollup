7c8e197df5e012026e314244d9bfd065e41ec2f4
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2024-10-24 23:41:35 +0200

    Fix proposer config for tests

--- packages/taiko-client/prover/prover_test.go
+++ packages/taiko-client/prover/prover_test.go
@@ -110,6 +110,10 @@ func (s *ProverTestSuite) SetupTest() {
 		L2SuggestedFeeRecipient:    common.HexToAddress(os.Getenv("L2_SUGGESTED_FEE_RECIPIENT")),
 		ProposeInterval:            1024 * time.Hour,
 		MaxProposedTxListsPerEpoch: 1,
+		GasNeededForProposingBlock: 0,
+		GasNeededForProvingBlock:   0,
+		PriceFluctuationModifier:   50,
+		OffChainCosts:              big.NewInt(0),
 	}, s.txmgr, s.txmgr))
 
 	s.proposer = prop
